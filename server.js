221c7374aed2d06436a95d9ccb1639b71d4cebf4
DiegoBandhy <diegoby1994@gmail.com>
2022-11-13 18:59:59 -0500

    desafio 4 con fallas. Avance desafio 5 y 6

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 // importar librería
-
-const express = require("express")
+const express = require('express');
 const router = express.Router();
+//const http = require('http');
+
+//const { Server } = require('socket.io');
+//const { engine } = require('express-handlebars')
+
+
+
+
 const { routerWeb } = require("./routers/routerWeb.js")
 const { routerApi } = require("./routers/routerApi.js")
+//SOCKET. NEcesita http en server y necesita evento en html
+
 
 //luego de definir funciones, paso a los controladores
-
+//const server = http.createServer(app);
+//const io = new Server(server)
 const app = express()
 app.use(express.static('views'));
 
@@ -16,6 +26,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
 
 app.use("/views", express.static("views"))
+
+
+
 
 //RUTAS
  app.use("/", routerWeb)
