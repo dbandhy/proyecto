3ced9f5bf0a68aedc1390a92faf64bf1f9e49561
DiegoBandhy <diegoby1994@gmail.com>
2022-11-26 09:22:22 -0500

    falta que aparezca contenido

---
+++ public/js/index.js
@@ -0,0 +1,159 @@
+const socket = io();
+        const btn = document.getElementById('btn-submit');
+        const btnMensaje = document.getElementById('btn-submit-mensaje');
+        
+        const inputTitle = document.getElementById('input-title');
+        const inputPrice = document.getElementById('input-price');
+        const inputImg = document.getElementById('input-img');
+        
+        btnMensaje.addEventListener('click', (evt) => {
+            evt.preventDefault();
+            const email = document.getElementById('input-email').value;
+            const mensaje = document.getElementById('input-mensaje').value;
+            
+            if (email !== '' && mensaje !== '') {
+            
+                socket.emit('nuevoMensaje', {
+                    "email" : email,
+                    "message" : mensaje,
+                    "date" : new Date().toLocaleString()
+                })
+            }
+            
+        })
+        
+        
+        btn.addEventListener('click', (evt) => {
+            
+            const title = inputTitle.value;
+            const price = inputPrice.value;
+            const img = inputImg.value;
+            
+            if (title !== '' && price !== '' && img !== '') {
+                socket.emit('productoAgregado',{
+                    "title": title,
+                    "price": price,
+                    "thumbnail": img
+                })
+            }
+            
+            
+        })
+        
+        socket.on('listaMensajesActualizada', (data) => {
+            console.log(data);
+            $('#historial-mensajes').empty();
+            data.forEach( (mensaje) => {
+                $('#historial-mensajes').append(
+                `
+                <small style="display:block"> - <em style="color:#9f9f9f">[${mensaje.date}]</em> <strong style="color:green">${mensaje.email}</strong>: ${mensaje.message} </small>
+                `
+                )
+            })
+        })
+        
+        socket.on('listaMensajesBienvenida', (data) => {
+            console.log(data);
+            $('#historial-mensajes').empty();
+            data.forEach( (mensaje) => {
+                $('#historial-mensajes').append(
+                `
+                <small style="display:block"> - <em style="color:#9f9f9f">[${mensaje.date}]</em> <strong style="color:green">${mensaje.email}</strong>: ${mensaje.message} </small>
+                `
+                )
+            })
+        })
+        
+        socket.on('listaActualizada', (data) => {
+            $('#table-body').empty();
+            data.forEach( (element) => {
+            
+                $('#table-body').append(
+                `
+                <tr>
+                    <td>${element.id}</td>
+                    <td>${element.title}</td>
+                    <td>${element.price}</td>
+                    <td>${element.thumbnail}</td>
+                </tr>
+                `
+                )
+            })
+            
+        })
+        
+        socket.on('bienvenidoLista', (data) => {
+            $('#table-body').empty();
+            data.forEach( (element) => {
+            
+                $('#table-body').append(
+                `
+                <tr>
+                    <td>${element.id}</td>
+                    <td>${element.title}</td>
+                    <td>${element.price}</td>
+                    <td>${element.thumbnail}</td>
+                </tr>
+                `
+                )
+            })
+            
+        })
+        
+
+// const socket = io()
+
+// function mostrarPeliculas(peliculas) {
+//     const peliculasParaMostrar =peliculas.map(({fecha, autor, pedido}) => {
+//         return `<li>${fecha} - ${autor} - ${pedido} </li>`
+//     })
+
+//     const peliculasHtml = 
+//     `<ul>${peliculasParaMostrar.join(`\n`)}</ul>`
+
+//     const listaPeliculas =document.getElementById('listaPeliculas')
+//     listaPeliculas.innerHTML = peliculasHtml
+
+// }
+
+// //SOCKET ENVÍO
+
+// socket.on('peliculasActualizadas', peliculas => {
+//     mostrarPeliculas(peliculas)
+// })
+
+// const botonEnviar =document.getElementById('botonEnviar')
+// botonEnviar.addEventListener('click', e => {
+//     const inputAutor = document.getElementById('inputAutor')
+//     const inputPedido = document.getElementById('inputPedido')
+
+//     if (inputAutor.value && inputPedido.value) {
+//         const mensaje = {
+//             autor: inputAutor.value,
+//             pedido: inputPedido.value
+//         }
+//         socket.emit('nuevoMensaje', mensaje)
+//     } else {
+//         alert('Ingrese pedido')
+//     }
+// })
+
+// socket.on('mi mensaje', data => {
+//     console.log('(cliente) ' + data)
+
+//     socket.emit('msg-cliente', `(cliente) recibi ${data}`)
+// })
+
+// socket.on('heartbeat', () => {
+//     console.log('todo OK')
+    
+// })
+
+// function saludar() {
+//     socket.emit('saludo', 'saludo desde el cliente')
+// }
+
+// const botonSaludar = document.getElementById('botonSaludar')
+// botonSaludar.addEventListener('click', e => {
+//     saludar()
+// })
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+
 // importar librería
 const express = require('express');
 const { Server: HttpServer } = require('http');
 const { Server: Socket } = require('socket.io')
+const { Archivo } = require('./contenedor/contenedor.js')
 
 const app = express()
 const httpServer = new HttpServer(app);
 const io = new Socket(httpServer)
 
-const router = express.Router();
 
+
+const router = express.Router();
 
 const { engine } = require('express-handlebars')
 
@@ -16,11 +19,13 @@ const { routerWeb } = require("./routers/routerWeb.js")
 const { routerApi } = require("./routers/routerApi.js")
 //SOCKET. NEcesita http en server y necesita evento en html
 
+let contenedor = new Archivo("peliculas.json");
+let chat = new Archivo("chat.txt")
 
 //luego de definir funciones, paso a los controladores
 
 
-app.use(express.static('views'));
+app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
@@ -32,61 +37,61 @@ app.engine('hbs', engine({
 }))
 
 
-app.get('/', () => {
-   res.send("OK")
-})
+// app.get('/', () => {
+//    res.send("OK")
+// })
 
-app.get('/peliculas', async(req, res) => {
-   const productos = await contenedor.leer();
-   res.render('pages/list', {peliculas})
-})
-
-app.post('/peliculas', async(req,res) => {
-   const {body} = req;
-   await contenedor.guardar(body);
-   res.redirect('/');
-})
 
 //const mensajes = []
 
-const personas = []
-//PARECIDO A MANEJAR EVENTOS
-io.on('connection', (socket) => {
+// const personas = []
+// //PARECIDO A MANEJAR EVENTOS
+// io.on('connection', (socket) => {
 
-   socket.emit('personas', personas)
+//    socket.emit('personas', personas)
 
-   socket.on('update', persona => {
-      persona.fecha = new Date().toLocaleString()
-      personas.push(persona)
-      io.sockets.emit('personas', personas)
-   })
-})
-
-
-// io.on('connection', socket => {
-//    //se ejecuta la primera vez que se abre una conexión
-//    console.log('usuario conectado' + socket.id)
-
-//    //se imprimirá solo la primera vez que se ejecuta una conexión
-//    socket.emit('mi mensaje', 'Este es el mensaje desde el servidor')
-
-//    socket.on('msg-cliente', data => {
-//       console.log(`(server) recibí: ${data} `)
+//    socket.on('update', persona => {
+//       persona.fecha = new Date().toLocaleString()
+//       personas.push(persona)
+//       io.sockets.emit('personas', personas)
 //    })
-   
-//    socket.on('saludo', data => {
-//       console.log(`(server) recibí: ${data} `)
-//    })
-//    io.sockets.emit('mi mensaje', 'esto lo recibe todo el mundo')
-      
-   
 // })
+
+io.on('connection', async(socket) => {
+   console.log('🟢 Usuario conectado')
+   
+   const peliculas = await contenedor.leer();
+   socket.emit('bienvenidoLista', peliculas )
+   
+   const mensajes = await chat.leer();
+   socket.emit('listaMensajesBienvenida', mensajes)
+   
+   socket.on('nuevoMensaje', async(data) => {
+       await chat.guardar(data);
+       
+       const mensajes = await chat.leer();
+       io.sockets.emit('listaMensajesActualizada', mensajes)
+   })
+
+   socket.on('productoAgregado', async(data) => {
+       console.log('Alguien presionó el click')
+       await archivo.guardar(data);
+       
+       const productos = await archivo.leer();
+       io.sockets.emit('listaActualizada', peliculas);
+   })
+   
+   socket.on('disconnect', () => {
+       console.log('🔴 Usuario desconectado')
+   })
+   
+})
 
 //middleware. Interpreta json y formularios
  app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
 
-app.use("/views", express.static("views"))
+app.use("/public", express.static("public"))
 
 // Multer config
 // const multer = require('multer');
@@ -105,8 +110,8 @@ app.use("/views", express.static("views"))
 
 //RUTAS
 // app.use("/", routerWeb)
- app.use("/api/peliculas", routerApi)
- app.use("/api/peliculas/id", routerApi)
+//  app.use("/api/peliculas", routerWeb)
+//  app.use("/api/peliculas/id", routerWeb)
 //  app.post("/imagenes", middlewareDeImagenes, (req, res) => {
 //    const file = req.file
 //    if(!file) {
@@ -116,114 +121,130 @@ app.use("/views", express.static("views"))
 //    res.send(`ARchivo <b> ${file.originalname}</b> subió de forma correcta`)
 //  })
 
+
+app.get('/peliculas', async(req, res) => {
+   const peliculas = await contenedor.leer();
+   res.render('pages/list', {peliculas})
+})
+
+app.post('/peliculas', async(req,res) => {
+   const {body} = req;
+   await contenedor.guardar(body);
+   res.redirect('/');
+})
+app.get('/', (req,res) => {
+   res.render('pages/form', {})
+})
+
+
  //LISTEN
  const server = httpServer.listen(8080, () => {
     console.log(`conectado al puerto ${server.address().port}`)
  })
 
 
+ server.on('error', (err) => console.log(err))
 
 
 
 
 
+// // // importar librería
+// // const express = require('express');
+// // const { Server: HttpServer } = require('http');
+// // const { Server: Socket } = require('socket.io')
 
-// // importar librería
-// const express = require('express');
-// const { Server: HttpServer } = require('http');
-// const { Server: Socket } = require('socket.io')
+// // const app = express()
+// // const httpServer = new HttpServer(app);
+// // const io = new Socket(httpServer)
 
-// const app = express()
-// const httpServer = new HttpServer(app);
-// const io = new Socket(httpServer)
-
-// const router = express.Router();
-
-
-// //const { engine } = require('express-handlebars')
-
-// const { routerWeb } = require("./routers/routerWeb.js")
-// const { routerApi } = require("./routers/routerApi.js")
-// //SOCKET. NEcesita http en server y necesita evento en html
+// // const router = express.Router();
 
 
-// //luego de definir funciones, paso a los controladores
+// // //const { engine } = require('express-handlebars')
+
+// // const { routerWeb } = require("./routers/routerWeb.js")
+// // const { routerApi } = require("./routers/routerApi.js")
+// // //SOCKET. NEcesita http en server y necesita evento en html
 
 
-// app.use(express.static('views'));
-
-// app.get('/', () => {
-//    res.send("OK")
-// })
-
-// const mensajes = []
-// //PARECIDO A MANEJAR EVENTOS
-// io.on('connection', (socket) => {
-
-//    socket.emit('mensajesActualizados', mensajes)
-
-//    socket.on('nuevoMensaje', mensaje => {
-//       mensaje.fecha = new Date().toLocaleString()
-//       mensajes.push(mensaje)
-//       io.sockets.emit('mensajesActualizados', mensajes)
-//    })
-// })
+// // //luego de definir funciones, paso a los controladores
 
 
-// // io.on('connection', socket => {
-// //    //se ejecuta la primera vez que se abre una conexión
-// //    console.log('usuario conectado' + socket.id)
+// // app.use(express.static('views'));
 
-// //    //se imprimirá solo la primera vez que se ejecuta una conexión
-// //    socket.emit('mi mensaje', 'Este es el mensaje desde el servidor')
+// // app.get('/', () => {
+// //    res.send("OK")
+// // })
 
-// //    socket.on('msg-cliente', data => {
-// //       console.log(`(server) recibí: ${data} `)
+// // const mensajes = []
+// // //PARECIDO A MANEJAR EVENTOS
+// // io.on('connection', (socket) => {
+
+// //    socket.emit('mensajesActualizados', mensajes)
+
+// //    socket.on('nuevoMensaje', mensaje => {
+// //       mensaje.fecha = new Date().toLocaleString()
+// //       mensajes.push(mensaje)
+// //       io.sockets.emit('mensajesActualizados', mensajes)
 // //    })
+// // })
+
+
+// // // io.on('connection', socket => {
+// // //    //se ejecuta la primera vez que se abre una conexión
+// // //    console.log('usuario conectado' + socket.id)
+
+// // //    //se imprimirá solo la primera vez que se ejecuta una conexión
+// // //    socket.emit('mi mensaje', 'Este es el mensaje desde el servidor')
+
+// // //    socket.on('msg-cliente', data => {
+// // //       console.log(`(server) recibí: ${data} `)
+// // //    })
    
-// //    socket.on('saludo', data => {
-// //       console.log(`(server) recibí: ${data} `)
-// //    })
-// //    io.sockets.emit('mi mensaje', 'esto lo recibe todo el mundo')
+// // //    socket.on('saludo', data => {
+// // //       console.log(`(server) recibí: ${data} `)
+// // //    })
+// // //    io.sockets.emit('mi mensaje', 'esto lo recibe todo el mundo')
       
    
-// // })
+// // // })
 
-// //middleware. Interpreta json y formularios
-//  app.use(express.json())
-// app.use(express.urlencoded({ extended:true }))
+// // //middleware. Interpreta json y formularios
+// //  app.use(express.json())
+// // app.use(express.urlencoded({ extended:true }))
 
-// app.use("/views", express.static("views"))
+// // app.use("/views", express.static("views"))
 
-// // Multer config
-// // const multer = require('multer');
-// // const storage = multer.diskStorage({
-// //    destination: function(req, file, cb) {
-// //       cb(null, 'uploads')
-// //    },
-// //    filename: function(req, file, cb) {
-// //       cb(null, `${Date.now()}-${file.originalname}`)
-// //    }
-// // })
+// // // Multer config
+// // // const multer = require('multer');
+// // // const storage = multer.diskStorage({
+// // //    destination: function(req, file, cb) {
+// // //       cb(null, 'uploads')
+// // //    },
+// // //    filename: function(req, file, cb) {
+// // //       cb(null, `${Date.now()}-${file.originalname}`)
+// // //    }
+// // // })
 
-// // const upload = multer({ storage: storage })
-// // const middlewareDeImagenes = upload.single("miArchivo")
+// // // const upload = multer({ storage: storage })
+// // // const middlewareDeImagenes = upload.single("miArchivo")
 
 
-// //RUTAS
-// // app.use("/", routerWeb)
-//  app.use("/api/peliculas", routerApi)
-//  app.use("/api/peliculas/id", routerApi)
-// //  app.post("/imagenes", middlewareDeImagenes, (req, res) => {
-// //    const file = req.file
-// //    if(!file) {
-// //       res.status(400)
-// //       return res.send("Error al subir archivo")
-// //    }
-// //    res.send(`ARchivo <b> ${file.originalname}</b> subió de forma correcta`)
+// // //RUTAS
+// // // app.use("/", routerWeb)
+// //  app.use("/api/peliculas", routerApi)
+// //  app.use("/api/peliculas/id", routerApi)
+// // //  app.post("/imagenes", middlewareDeImagenes, (req, res) => {
+// // //    const file = req.file
+// // //    if(!file) {
+// // //       res.status(400)
+// // //       return res.send("Error al subir archivo")
+// // //    }
+// // //    res.send(`ARchivo <b> ${file.originalname}</b> subió de forma correcta`)
+// // //  })
+
+// //  //LISTEN
+// //  const server = httpServer.listen(8080, () => {
+// //     console.log(`conectado al puerto ${server.address().port}`)
 // //  })
-
-//  //LISTEN
-//  const server = httpServer.listen(8080, () => {
-//     console.log(`conectado al puerto ${server.address().port}`)
-//  })
